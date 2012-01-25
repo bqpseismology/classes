01253aad3df5207d3d5cbbdb9a9a25c1948c27f6
Carl Tape <carltape@gmail.com>
2012-01-25 18:51:30 +0000

    added example in hw1.m to show how to plot multiple items with semilogy (or loglog)
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@208 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- seis2012/hw1.m
+++ seis2012/hw1.m
@@ -8,12 +8,42 @@ clc
 clear
 close all
 
+%---------------------------
+
+% example to show one way to plot multiple items with log-scaled axes
+% (1) generate fake data and a best-fitting line
+% (2) plot lines
+n = 50;
+x = linspace(4,10,n)';
+b = 1;
+a = 9;
+e = 0.1;
+err = e*(-1+2*randn(n,1));
+Nd = 10.^(a-b*x + err);
+N  = 10.^(a-b*x);
+N2  = 10.^(a-1-b*x);
+
+figure;
+h1 = semilogy(x,Nd,'bV','markersize',8,'markerfacecolor','w');
+hold on;    % after this you do not need semilogy -- it is automatic
+h2 = plot(x,N,'k-');
+h3 = plot(x,N2,'r-');
+legend([h1 h2 h3],'fake data','cumulative fit','incremental fit');
+set(gca,'xtick',4:9);
+set(gca,'ytick',10.^[-1:5],'yticklabel',{'0.1','1','10','100','1000','10000','1000000'});
+xlabel('Moment magnitude, Mw'); ylabel('Number of fake earthquakes');
+title('dummy plot to show one way of plotting multiple things with log-y axes');
+
+%---------------------------
+% HW1, Problem 1-1
+
 % load GCMT catalog
 idir = './data/';       % you might need to change the path to data
 load([idir 'cmtall_sub']);
 whos
 
 % plot the catalog, colored by depth
+% note: use wrapTo360 to center the map on longitude=180
 [~,isort] = sort(dep,'descend');    % plot deepest events on top
 msize = 4^2;                        % marker size
 figure;
@@ -23,14 +53,18 @@ xlabel('Longitude'); ylabel('Latitutde');
 title(sprintf('GCMT catalog (%i events), colored by depth (km)',length(dep)));
 caxis([0 600]); colorbar
 
+% plot deepest events only
+%dcut = 600;
 %figure;
-%plot(wrapTo360(lon(dep > 600)),lat(dep > 600),'.');
+%plot(wrapTo360(lon(dep > dcut)),lat(dep > dcut),'.');
 %axis([0 360 -90 90]);
 %xlabel('Longitude'); ylabel('Latitutde');
 
 %---------------------------
+% HW1, Problem 1-2
 
-dmag = 0.1;
+dmag = 0.1;     % magnitude bin width
 % note: seis2GR.m is not a built-in matlab command
 [Ncum,N,Medges] = seis2GR(Mw,dmag);
 
+%==========================================================================
