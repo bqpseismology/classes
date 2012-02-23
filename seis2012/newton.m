5d49a20dec57b34a6740dca4c92f0bdd71c9e951
Carl Tape <carltape@gmail.com>
2012-02-23 20:42:29 +0000

    minor update to template script newton.m
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@233 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- seis2012/newton.m
+++ seis2012/newton.m
@@ -9,7 +9,7 @@
 % LAB EXERCISE:
 %   1. Using 'fminbnd' compute the (numerical) minimum of F(m).
 %         Plot the point (mmin,F(mmin)) on the curve F(m).
-%   2. Implement the Newton algorithm and show that you reach the minimum.
+%   2. Implement the Newton algorithm, and show that you reach the minimum.
 %         Plot the point (m,F(m)) for each new value of m.
 %         Try several different starting values of m to make sure it works.
 %   3. Define a sensible stopping criterion.
@@ -23,18 +23,20 @@ clc
 format long
 
 % define a misfit function and its derivatives
+% note: our 'model vector' is one-dimensional
 P = [2 -1 2 -1 2];          % non-quadratic
+%P = [0  0 2 -1 2];         % quadratic
 %F = @(m) ( polyval(P,m) );
-F   = @(m) ( P(1)*m.^4 + P(2)*m.^3 + P(3)*m.^2 + P(4)*m + P(5) );
-dF1 = @(m) ( 4*P(1)*m.^3 + 3*P(2)*m.^2 + 2*P(3)*m + P(4) );
-dF2 = @(m) ( 12*P(1)*m.^2 + 6*P(2)*m + 2*P(3) );
+F = @(m) ( P(1)*m.^4 + P(2)*m.^3 + P(3)*m.^2 + P(4)*m + P(5) );
+g = @(m) ( 4*P(1)*m.^3 + 3*P(2)*m.^2 + 2*P(3)*m + P(4) );
+H = @(m) ( 12*P(1)*m.^2 + 6*P(2)*m + 2*P(3) );
 
 % specify bounds for choosing initial model (and for plotting)
 mA = -2;
 mB = 2;
 m = linspace(mA,mB,100);
 
-% COMPUTE MINIMUM HERE
+% COMPUTE MINIMUM OF MISFIT FUNCTION HERE
 
 figure; hold on;
 plot(m,F(m));
@@ -45,8 +47,8 @@ xlabel('model m'); ylabel('misfit function, F(m)');
 
 % initial model
 %m0 = -1.5;
-m0 = mA + (mB-mA)*rand;
+m0 = mA + (mB-mA)*rand; % random starting value
 
-% IMPLEMENT NEWTON ALGORITHM HERE
+% IMPLEMENT NEWTON ALGORITHM HERE (see class notes on Least Squares)
 
 %==========================================================================
