4919cd1d1013bbcbc5f95407b53a060e533a8db4
Carl Tape <carltape@gmail.com>
2013-03-28 23:53:25 +0000

    minor updates to ex_2_1_2_cht.m
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@873 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- inv2013/ex_2_1_2_cht.m
+++ inv2013/ex_2_1_2_cht.m
@@ -13,26 +13,28 @@ ddir = '/usr/local/matlab_toolboxes/aster/cd_5.2/Examples/chap2/ex_2_1_2/';
 load([ddir 'data1.mat']);
 t = data1(:,1);
 y = data1(:,2);
-sigma = data1(:,3);
+sigvec = data1(:,3);
 N = length(t);
 M = 3;
 
-disp('displaying t,y,sigma')
-[t , y , sigma ]
+disp('displaying t, y, sigma')
+[ t y sigvec ]
 
 % build the parabolic system matrix
 G = [ ones(N,1) t -0.5*t.^2 ];
 
 % apply the weighting
-yw = y ./ sigma;
-Gw = G ./ [sigma sigma sigma];
+yw = y ./ sigvec;
+Gw = G ./ [sigvec sigvec sigvec];  % divide each row by an element in sigvec
 
 % solve for the least-squares solution
 disp('Least-squares solution')
-m = inv(Gw'*Gw)*Gw'*yw
+%m = inv(Gw'*Gw)*Gw'*yw
+%m = G\y
+m = Gw\yw
 
 % get the covariance matrix
-ginv = inv(Gw'*Gw)*Gw';
+ginv = inv(Gw'*Gw)*Gw';     % generalized inverse, pinv(Gw)
 disp('Covariance matrix')
 covm = ginv*ginv'
 
@@ -44,7 +46,8 @@ dm = 1.96*sqrt(diag(covm));
 % N-M degrees of freedom
 dof = N-M;
 disp(sprintf('Chi-square misfit for %i degrees of freedom (dof)',dof));
-chi2 = norm((y - G*m)./sigma)^2
+chi2 = norm((y - G*m)./sigvec)^2
+%chi2 = norm(yw - Gw*m)^2
 
 % Find the p-value for this data set
 disp('chi-square p-value')
@@ -61,7 +64,7 @@ mm = m(1) + m(2)*xx - 0.5*m(3)*xx.^2;
 
 figure(1); hold on
 plot(xx,mm,'k');
-errorbar(t,y,sigma,'ko');
+errorbar(t,y,sigvec,'ko');
 xlabel('Time (s)');
 ylabel('Elevation (m)');
 %print -deps2 c2fparabfig.eps
@@ -69,7 +72,7 @@ ylabel('Elevation (m)');
 % Output covm and the eigenvalues/eigenvectors of covm.
 disp('Covariance matrix for fitted parameters:')
 covm
-disp('Eigenvalues/eigenvectors of the covariance matrix:');
+disp('Eigenvalues/eigenvectors of the inverse covariance matrix:');
 [u,lam] = eig(inv(covm))
 disp('95% confidence ellipsoid semiaxis lengths:');
 semi_axes = [sqrt(chi2inv(0.95,3)*(1./diag(lam)))]'
@@ -78,19 +81,23 @@ disp('95% confidence ellipsoid semiaxes:')
 [semi_axes(1)*u(:,1), semi_axes(2)*u(:,2), semi_axes(3)*u(:,3)]
 
 %-------------------------------
-
 % Monte Carlo Section
-y0 = G*m; 
 
-nsamp = 1e3;
+% predictions for baseline model
+y0 = G*m;
 
-for nreal = 1:nsamp
+nsamp = 1e3;    % number of samples
+mmc = zeros(3,nsamp);
+chimc = zeros(nsamp,1);
+for ii = 1:nsamp
   % Generate a trial data set of perturbed, weighted data
-  ytrial = y0 + sigma.*randn(N,1);
-  ywtrial = ytrial./sigma;
-  mmc(nreal,:) = (Gw\ywtrial)';
+  ytrial = y0 + sigvec.*randn(N,1);
+  ywtrial = ytrial./sigvec;
+  mtrial = Gw\ywtrial;
   % Eq. 2.19
-  chimc(nreal) =  norm( (G*mmc(nreal,:)' - ytrial) ./ sigma )^2;
+  chimc(ii) = norm( (G*mtrial - ytrial) ./ sigvec )^2;
+  %chimc(ii) = norm( ywtrial - Gw*mtrial )^2;
+  mmc(:,ii) = mtrial;
 end
 
 % Plot the histogram of chi squared values
@@ -118,9 +125,9 @@ axis([0 xmax 0 ymax]);
 
 % Plot the histograms of the model parameters
 figure(3)
-subplot(1,3,1); hist(mmc(:,1)); title('m_1 (m)')
-subplot(1,3,2); hist(mmc(:,2)); title('m_2 (m/s)')
-subplot(1,3,3); hist(mmc(:,3)); title('m_3 (m/s^2)')
+subplot(1,3,1); hist(mmc(1,:)); title('m_1 (m)')
+subplot(1,3,2); hist(mmc(2,:)); title('m_2 (m/s)')
+subplot(1,3,3); hist(mmc(3,:)); title('m_3 (m/s^2)')
 
 % Plot the realizations of each pair of model parameters with the other
 figure; nr=3; nc=2;
@@ -129,17 +136,17 @@ ax2 = [-50 50 7 12];
 ax3 = [80 120 7 12];
 
 subplot(nr,nc,1)
-plot(mmc(:,1),mmc(:,2),'k.')
+plot(mmc(1,:),mmc(2,:),'k.')
 xlabel('m_1 (m)'); ylabel('m_2 (m/s)')
 axis(ax1);
 
 subplot(nr,nc,3)
-plot(mmc(:,1),mmc(:,3),'k.')
+plot(mmc(1,:),mmc(3,:),'k.')
 xlabel('m_1 (m)'); ylabel('m_3 (m/s^2)')
 axis(ax2);
 
 subplot(nr,nc,5)
-plot(mmc(:,2),mmc(:,3),'k.')
+plot(mmc(2,:),mmc(3,:),'k.')
 xlabel('m_2 (m/s)'); ylabel('m_3 (m/s^2)')
 disp('Displaying Projections of 1000 Monte-Carlo models (fig 4)')
 axis(ax3);
