a5f9eac5a1aa2bd3044aae9f9e2c246e12308b5f
Carl Tape <carltape@gmail.com>
2013-04-12 01:29:10 +0000

    adjusted plotting script to show NaN as grey when using imagesc
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@928 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- inv2013/mogi/plot_model.m
+++ inv2013/mogi/plot_model.m
@@ -1,4 +1,4 @@
-function plot_model(modeloutput)
+function plot_model(D)
 
 % plot an interferogram (both as an unwrapped image and wrapped phase image
 
@@ -6,15 +6,50 @@ SAMPLE = 1100;
 LINE = 980;
 POSTING = 40.0;
 HALF_WAVE = 28.3;
+
 load data_mask
+
+xvec = [1:POSTING:1100*POSTING]./1000;
+yvec = [1:POSTING:LINE.*POSTING]./1000;
+
+nanclr = 0.5*[1 1 1];
+
 figure;
-subplot(2,1,1);imagesc([1:POSTING:1100*POSTING]./1000,[1:POSTING:LINE.*POSTING]./1000,modeloutput.*data_mask);colorbar;caxis([-30 30])
+
+fvec = D.*data_mask;
+
+subplot(2,1,1);
+imagesc(xvec,yvec,fvec);
+
+% http://stackoverflow.com/questions/8481324/contrasting-color-for-nans-in-imagesc
+cm = colormap('jet');
+amin = min(D(:));
+amax = max(D(:));
+n = size(cm,1);
+dmap = (amax-amin)/n;
+% add nan color to colormap
+colormap([nanclr; cm]);
+% changing color limits
+caxis([amin-dmap amax]);
+% place a colorbar
+hcb = colorbar;
+% change Y limit for colorbar to avoid showing NaN color
+ylim(hcb,[amin amax])
+
+%caxis([-30 30])
 axis equal; axis tight
 set(gca,'FontSize',12); h = xlabel('Easting [km]');set(h,'FontSize',14);
 h = ylabel('Northing [km]');set(h,'FontSize',14);
 h = title('Line of Sight Motion [mm]');set(h,'FontSize',14);
 
-subplot(2,1,2);imagesc([1:POSTING:1100*POSTING]./1000,[1:POSTING:LINE.*POSTING]./1000,wrap(modeloutput.*data_mask./10)./5.66.*4*pi);colorbar;axis equal; axis tight
+fvec = wrap(D.*data_mask./10)./5.66.*4*pi;
+
+subplot(2,1,2);
+imagesc(xvec,yvec,fvec);
+colorbar;
+axis equal; axis tight
 set(gca,'FontSize',12); h = xlabel('Easting [km]');set(h,'FontSize',14);
 h = ylabel('Northing [km]');set(h,'FontSize',14);
 h = title('Interferogram phase [rad]');set(h,'FontSize',14);
+
+
--- inv2013/mogi/read_data.m
+++ inv2013/mogi/read_data.m
@@ -1,6 +1,7 @@
 clear all; close all;
-% unwrapped interferogram of Okmok volcano (pixel values represent line-of-sight (LOS) range change in radians
-%zeros in this interferogram represent data voids
+% unwrapped interferogram of Okmok volcano
+% pixel values represent line-of-sight (LOS) range change in radians
+% zeros in this interferogram represent data voids
 
 filename = './E451_20000818_20020719'; 
 
@@ -10,12 +11,12 @@ LINE = 980;
 POSTING = 40.0;
 HALF_WAVE = 28.3;
 
-pha_file=[filename,'.unw'];
-obs_rngchg=read_real_be(pha_file,LINE,SAMPLE);
-obs_rngchg=obs_rngchg*HALF_WAVE/2.0/pi;
+pha_file = [filename,'.unw'];
+obs_rngchg = read_real_be(pha_file,LINE,SAMPLE);
+obs_rngchg = obs_rngchg*HALF_WAVE/2.0/pi;
 obs_rngchg(obs_rngchg==0) = nan;
 data_mask = isnan(obs_rngchg);
-data_mask = data_mask == 0;
-save data_mask data_mask
+data_mask = (data_mask==0);
+save data_mask
 
 plot_model(obs_rngchg)
