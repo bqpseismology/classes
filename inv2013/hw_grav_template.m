f6d9f530ebc8c8e341c6e83184fc618fb1dd7525
Carl Tape <carltape@gmail.com>
2013-04-18 07:27:22 +0000

    updated the gravity data for TSVD
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@947 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- inv2013/hw_grav_template.m
+++ inv2013/hw_grav_template.m
@@ -9,17 +9,26 @@ clear
 close all
 clc
 
-% directory for input data
-dir0 = './';
-ddir = [dir0 'data/'];
+% vector of gravity observations
+d = [
+   3.200806372256319
+   4.281623543773417
+   5.000263689234144
+   6.083743151550228
+   6.755930945340174
+   6.643468068035755
+   6.585278806244575
+   5.953388158640383
+   5.123776695089963
+   4.247191182133855
+   3.175260569937137
+   2.511125551747691
+   2.004733630167365
+   1.333121959786504
+   0.949130251286033 ];
 
-%==========================================================================
-
-% get data and discretized integral
-load([ddir 'gravity.dat']);
-d = gravity;
-n = length(d);
-m = n;
+n = length(d);  % number of data
+m = n;          % number of model parameters
 
 % construct discretization vector for model
 xmin = 0;
