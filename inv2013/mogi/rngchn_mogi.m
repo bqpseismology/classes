076351574307c2683e80d957d911ccc1959da32e
Carl Tape <carltape@gmail.com>
2013-04-22 05:45:43 +0000

    added comments to mogi scripts to try to understand the convention for basis and angles and line-of-sight vector
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@959 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- inv2013/mogi/rngchn_mogi.m
+++ inv2013/mogi/rngchn_mogi.m
@@ -30,49 +30,62 @@ function [del_rng] = rngchn_mogi(n1,e1,depth,del_v,ning,eing,plook)
 dsp_coef = (1000000*del_v*3)/(pi*4);
 
 if(mm==1 && n==1)
-  disp('Calculating a matrix of rngchg values')
-  del_rng = zeros(m,nn);
-  del_d = del_rng;
-  del_f = del_rng;
-  tmp_n = del_rng;
-  tmp_e = del_rng;
-  for i_loop = 1:m
-    tmp_e(i_loop,:) = eing;
-  end
-  for i_loop = 1:nn
-    tmp_n(:,i_loop) = ning;
-  end
-  d_mat = sqrt((tmp_n-n1).^2 + (tmp_e-e1).^2);
-  tmp_hyp = ((d_mat.^2 + depth.^2).^1.5);
-  del_d = dsp_coef*d_mat./tmp_hyp;
-  del_f = dsp_coef*depth./tmp_hyp;
-  azim = atan2((tmp_e-e1),(tmp_n-n1));
-  e_disp = sin(azim).*del_d;
-  n_disp = cos(azim).*del_d;
-  for i_loop = 1:nn
-    del_rng(:,i_loop) = [e_disp(:,i_loop),n_disp(:,i_loop),...
-                       del_f(:,i_loop)]*plook';
-  end
-  del_rng = -1.0*del_rng;
+    %disp('Calculating a matrix of rngchg values')
+    del_rng = zeros(m,nn);
+    %del_d = del_rng;
+    %del_f = del_rng;
+    tmp_n = del_rng;
+    tmp_e = del_rng;
+    for i_loop = 1:m
+        tmp_e(i_loop,:) = eing;
+    end
+    for i_loop = 1:nn
+        tmp_n(:,i_loop) = ning;
+    end
+    % horizontal distance
+    d_mat = sqrt((tmp_n-n1).^2 + (tmp_e-e1).^2);
+    % denominator of displacement field for mogi source
+    tmp_hyp = ((d_mat.^2 + depth.^2).^1.5);
+    % Uh, horizontal displacement 
+    del_d = dsp_coef*d_mat./tmp_hyp;
+    % Uz
+    del_f = dsp_coef*depth./tmp_hyp;
+    % azimuthal angle
+    azim = atan2((tmp_e-e1),(tmp_n-n1));
+    % compute Ue and Un using Uh and azimuthal angle
+    e_disp = sin(azim).*del_d;
+    n_disp = cos(azim).*del_d;
+    % project displacement field onto look vector
+    for i_loop = 1:nn
+        del_rng(:,i_loop) = ...
+            [e_disp(:,i_loop) n_disp(:,i_loop) del_f(:,i_loop)]*plook';
+    end
+    % flip sign projected distance (or flip plook)
+    del_rng = -1.0*del_rng;
+    
 elseif ((mm==1 && m==1) || (n==1 && nn==1))
-  if (n~=nn)
-    error('Coord vectors not equal length!')
-  end
-%  disp('Calculating a vector of rngchng values')
-  del_rng = zeros(size(ning));
-  del_d = del_rng;
-  del_f = del_rng;
-  d_mat = sqrt((ning-n1).^2 + (eing-e1).^2);
-  tmp_hyp = ((d_mat.^2 + depth.^2).^1.5);
-  del_d = dsp_coef*d_mat./tmp_hyp;
-  del_f = dsp_coef*depth./tmp_hyp;
-  azim = atan2((eing-e1),(ning-n1));
-  e_disp = sin(azim).*del_d;
-  n_disp = cos(azim).*del_d;
-%  disp([e_disp, n_disp, del_f])
-  del_rng = [e_disp, n_disp, del_f]*plook';
-  del_rng = -1.0*del_rng;
+    if (n~=nn)
+        error('Coord vectors not equal length!')
+    end
+    %disp('Calculating a vector of rngchng values')
+    % horizontal distance
+    d_mat = sqrt((ning-n1).^2 + (eing-e1).^2);
+    % denominator of displacement field for mogi source
+    tmp_hyp = ((d_mat.^2 + depth.^2).^1.5);
+    % Uh, horizontal displacement
+    del_d = dsp_coef*d_mat./tmp_hyp;
+    % Uz
+    del_f = dsp_coef*depth./tmp_hyp;
+    % azimuthal angle
+    azim = atan2((eing-e1),(ning-n1));
+    % compute Ue and Un using Uh and azimuthal angle
+    e_disp = sin(azim).*del_d;
+    n_disp = cos(azim).*del_d;
+    % project displacement field onto look vector
+    del_rng = [e_disp n_disp del_f]*plook';
+    % flip sign of projected distance (or flip plook)
+    del_rng = -1.0*del_rng;
+    
 else
-  error('Coord vectors make no sense!')
+    error('Coord vectors make no sense!')
 end
-
