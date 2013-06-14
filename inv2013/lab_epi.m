b83459ad954a046d74fa8afc9fce150fdc33c34b
Carl Tape <carltape@gmail.com>
2013-06-14 22:17:55 +0000

    changed notation from f to p to denote probability density
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@1058 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- inv2013/lab_epi.m
+++ inv2013/lab_epi.m
@@ -16,15 +16,15 @@
 clear, close all, clc
 
 % limits for x to consider
-% OUTSIDE THESE LIMITS WE ASSUME THAT f(x) = 0
+% OUTSIDE THESE LIMITS WE ASSUME THAT p(x) = 0
 xmin = -15; xmax = 12;
 
-% define in-line function f(x)
+% define in-line function p(x)
 x1 = -2; A1 = 2; sig1 = 2;
 x2 =  4; A2 = 1; sig2 = 0.5;
 % note that parameters x0 and A must be previously defined
-%f = @(x) ( A1*exp(-(x-x1).^2/(2*sig1^2)) );
-f = @(x) ( A1*exp(-(x-x1).^2/(2*sig1^2)) + A2*exp(-(x-x2).^2/(2*sig2^2)) );
+p = @(x) ( A1*exp(-(x-x1).^2/(2*sig1^2)) );
+%p = @(x) ( A1*exp(-(x-x1).^2/(2*sig1^2)) + A2*exp(-(x-x2).^2/(2*sig2^2)) );
 
 % KEY TECHNICAL POINT: f is a function, not a numerical array
 % (note that x is not a stored array)
@@ -35,35 +35,37 @@ NTRY = 1e5;
 xtry = xmin + (xmax-xmin)*rand(NTRY,1);
 
 % sample the function
-fmax = max([A1 A2]);
-ftry = f(xtry) / fmax;          % SET A: values between 0 and 1
+pmax = max([A1 A2]);
+ptry = p(xtry) / pmax;          % SET A: values between 0 and 1
 chance = rand(NTRY,1);          % SET B: values between 0 and 1
 
 % KEY COMMAND: compare pairs of test samples in sets A and B,
 %              then accept or reject the test sample
-ikeep = find(ftry > chance);
+ikeep = find(ptry > chance);
 xkeep = xtry(ikeep);
 
 % plot
 xcurve = linspace(xmin,xmax,1000);
-fcurve = f(xcurve);
+pcurve = p(xcurve);
 figure; nr=3; nc=2;
 edges1 = [xmin:0.2:xmax];
 edges2 = [0:0.05:1];
-subplot(nr,nc,1); plot(xcurve,fcurve/fmax);
-xlabel('x'); ylabel('f(x)'); title('(a)'); axis([xmin xmax 0 1.2]);
+subplot(nr,nc,1); plot(xcurve,pcurve/pmax);
+xlabel('x'); ylabel('p(x)'); title('(a)'); axis([xmin xmax 0 1.2]);
 subplot(nr,nc,2); plot_histo(xtry,edges1);
 xlabel('xtry'); title('(b)'); 
-subplot(nr,nc,3); plot_histo(ftry,edges2);
-xlabel('f(xtry)'); title('(c)'); 
+subplot(nr,nc,3); plot_histo(ptry,edges2);
+xlabel('p(xtry)'); title('(c)'); 
 subplot(nr,nc,4); plot_histo(chance,edges2);
 xlabel('chance'); title('(d)'); 
 subplot(nr,nc,5); plot_histo(xkeep,edges1);
 xlabel('xkeep');  title('(e)'); xlim([xmin xmax]);
 
-% if f is a probability density, -ln(f) is the misfit function: f = exp(-F(x))
-subplot(nr,nc,6); plot(xcurve,-log(fcurve));
-axis([xmin xmax -1 1.1*max(-log(fcurve))]);
-xlabel('x'); ylabel('F(x) = -ln(f(x))'); title('(f)');
+% if p is a probability density and F is the misfit function, then
+%    p(x) = exp(-F(x))
+%    F(x) = -ln(p(x))
+subplot(nr,nc,6); plot(xcurve,-log(pcurve));
+axis([xmin xmax -1 1.1*max(-log(pcurve))]);
+xlabel('x'); ylabel('F(x) = -ln(p(x))'); title('(f)');
 
 %==========================================================================
