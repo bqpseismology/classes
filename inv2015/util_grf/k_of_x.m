178aff1efc99b9ae761f2bd39d1740f7a531e5f5
Carl Tape <carltape@gmail.com>
2017-10-17 20:11:15 -0800

    remove old class dirs; rename to inverse and seismo

--- inv2015/util_grf/k_of_x.m
+++
@@ -1,8 +0,0 @@
-function k = k_of_x(x)
-% Wave vector from x-vector
-%
-N      = max(size(x));
-dx     = x(2)-x(1);
-dk     = (2*pi)/(N*dx);
-inull  = N/2;
-k      = dk*(linspace(1,N,N)-inull);
--- seis2014/earthfun_love_template.m
+++
@@ -1,8 +0,0 @@
-function [rho,mu] = earthfun_love(r)
-% earthfun_love return a rho and mu value for a specified radius r
-%
-% called by stress_disp_love.m
-
-global rspan crho cmu mrho mmu cthick
-
-% ENTER YOUR CODE HERE
